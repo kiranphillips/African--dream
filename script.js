775de6f9fb6ae07b7ee1d439cbb6b703b536caf1
Kiran Phillips <Kiranphillips61@gmail.com>
2022-05-09 13:41:03 -0600

    commit tree 5619e73e073f556a005b18c9f5c0465ddbf24392

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript"
+
+h2.textContent = "My name is Kiran Phillips, I was originally born in Zimbabwe and since lived in 7 countries before settling in South Africa. I have been working in hospitality for many years but made the leap to the tech world and pursued Software engineering through Flatiron.In this page you will learn about me and how I got to this point Infront of you."
 document.querySelector("body").appendChild(h2);
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3500); // Change image every 2 seconds
+}
